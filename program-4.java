1ed1dd99d4b251329954dbfe05f34207ff5eb09d
MUKESHMEENAKSHI <mukeshmeenakshi3001@gmail.com>
2025-08-11 16:33:34 +0530

    program-4.java

---
+++ program-4.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 8, 9, 12, 46, 76, 82, 15, 20, 30};
+
+        int[] counts = new int[9];  
+
+        for (int i = 1; i <= 9; i++) {
+            int count = 0;
+            for (int num : arr) {
+                if (num % i == 0) {
+                    count++;
+                }
+            }
+            counts[i - 1] = count; 
+        }
+
+        /
+        System.out.print("{");
+        for (int i = 1; i <= 9; i++) {
+            System.out.print(i + ": " + counts[i - 1]);
+            if (i < 9) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("}");
+    }
+}
